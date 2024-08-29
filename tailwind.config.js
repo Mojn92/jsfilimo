b558458a327a15eb5ed29527580b739901b90919
mojtaba <monjedu92@gmail.com>
2024-08-29 23:29:22 +0330

    Howtofixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,15 @@ export default {
     screens :{
       
       t1: " 680px ",
-      // => @media (min-width: 768px) 
+      // => @media (min-width: 680px) 
 
+      t2: " 768px ",
+      // => @media (min-width:768px) 
 
+      t3: " 800px ",
+      // => @media (min-width:800px) 
+
+      d1: " 1300px"
 
 
     }
