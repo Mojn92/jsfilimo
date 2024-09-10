ebf76a36a98761745f8981d1a24b9588f1327ea0
mojtaba <monjedu92@gmail.com>
2024-09-10 14:33:44 +0330

    paginationfixed

--- public/JS/main.js
+++ public/JS/main.js
@@ -20,6 +20,8 @@ for (let i = 1; i <= 4; i++) {
 let slide = document.querySelectorAll('.slide');
 let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
+const pages = document.querySelectorAll('.spanp');
+
 let counter = 0;
 slide.forEach((item, index) => {
     item.style.transform = `translatex(${index * 100}%)`
@@ -30,18 +32,52 @@ function counterslide(c) {
         item.style.transform = `translatex(${(index - c) * 100}%)`
     })
 
+
+    // disable
+    if (c == 0) {
+        next.style.opacity = "0.2"
+
+    }else{
+        next.style.opacity = "1"
+    } if (c == slide.length - 1) {
+        prev.style.opacity = "0.2"
+    }else{
+        prev.style.opacity = "1"
+    }
+
+    // pagination
+    pages.forEach((page, index) => {
+
+        
+        if (index === c) {
+
+            page.style.backgroundColor = "white";
+
+        }else{
+            page.style.backgroundColor = "black";
+        }
+    });
+
+
 }
 
-counterslide(counter)
 
 next.addEventListener("click", () => {
-    counter--;
-    counterslide(counter);
+    if (counter != 0) {
+        counter--;
+        counterslide(counter);
+        
+    }
+
 })
 
 prev.addEventListener("click", () => {
-    counter++;
-    counterslide(counter);
+    if (counter != slide.length - 1) {
+        counter++;
+        counterslide(counter);
+        
+    }
+
 })
 
 
@@ -55,44 +91,3 @@ prev.addEventListener("click", () => {
 
 // ----------------T1-Carouserl---------------------------
 
-
-// ----------------------Pagination-Js--------------------
-function Pagination() {
-    const next = document.querySelector('.next');
-    const prev = document.querySelector('.prev');
-    const pages = document.querySelectorAll('.spanp:not(.prev):not(.next)');
-    let currentPage = 0;
-
-    function updatePagination() {
-        pages.forEach((page, index) => {
-            
-                page.style.backgroundColor = "black";
-            if (index === currentPage) {
-              
-                page.style.backgroundColor = "white";
-
-            }
-        });
-    }
-
-    next.addEventListener('click', () => {
-        if (currentPage < pages.length - 1) {
-            currentPage++;
-            updatePagination();
-        }
-    });
-
-    prev.addEventListener('click', () => {
-        if (currentPage > 0) {
-            currentPage--;
-            updatePagination();
-        }
-    });
-
-    updatePagination(); // Initial call to set the active page
-}
-
-Pagination();
-
-
-// ----------------------Pagination-Js--------------------
