f0f952aacda9985f6c8e39a5f56ed19cc453d42f
mojtaba <monjedu92@gmail.com>
2024-09-19 17:18:46 +0330

    uptobestmovies

--- public/JS/t1Slider.js
+++ public/JS/t1Slider.js
@@ -1,73 +1,156 @@
-// ----------------T1-Slider---------------------------
-function t1Slider() {
-    let slide = document.querySelectorAll('.slide-min-0');
-    let next = document.querySelector('.next-0');
-    let prev = document.querySelector('.prev-0');
-    const pages = document.querySelectorAll('.spanp');
-    let counter = 0;
-    slide.forEach((item, index) => {
-        item.style.transform = `translatex(${index * 100}%)`
-    })
+function createSlider(
+  selector,
+  {
+    Pagination = false,
+    DisableNavigation = false,
+    SpaceBetween = 0,
+    interval = 4000,
+    autoplay = false,
+    transitionSpeed = 600,
+    slidesPerView = 1,
+    loop = false,
+    responsive = {
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  }
+) {
+  let x = selector + " .slide";
+  let slide = document.querySelectorAll(x);
+  x = selector + " .next";
+  let next = document.querySelector(x);
+  x = selector + " .prev";
+  let prev = document.querySelector(x);
+  x = selector + " .pagination";
+  let pagination = document.querySelectorAll(x);
+  let removeButton1 = document.querySelector(".remove-button-1");
+  let removeButton2 = document.querySelector(".remove-button-2");
+  let counter = 0;
 
-    function counterslide(c) {
-        slide.forEach((item, index) => {
-            item.style.transform = `translatex(${(index - c) * 100}%)`
-        })
+  slide.forEach((item, index) => {
+ 
+    item.style.marginLeft = `${SpaceBetween}px`;
+    item.style.width = `calc( (100% / ${slidesPerView}) - ${SpaceBetween}px)`;
+
+        // item.style.marginLeft = `${SpaceBetween}px`;
+        // item.style.width = `calc(${
+        //   (item.parentElement.parentElement.clientWidth /
+        //     slidesPerView /
+        //     item.parentElement.parentElement.clientWidth) *
+        //   100
+        // }%)`;
+
+  });
 
 
-        // disable
-        if (c == 0) {
-            next.style.opacity = "0.2"
 
+
+  slide[0].parentElement.style.width = `calc(100% /
+    ${slidesPerView} * ${slide.length})`;
+
+
+
+
+  
+  
+
+  function counterSlider(counter) {
+    // slide.forEach((item, index) => {
+    //   item.style.transform = `translatex(calc(${(index - counter) * 100}% + ${
+    //     SpaceBetween * (index - counter)
+    //   }px))`;
+    // });
+
+    // slide.forEach((item, index) => {
+    //   item.style.transform = `translatex(calc(${(index - counter) * 100}% + ${
+    //     SpaceBetween * (index - counter)
+    //   }px))`;
+    // });
+
+    slide[0].parentElement.style.transform = `translateX(calc(100% /
+    ${slide.length} * ${counter})`;
+
+
+    
+
+
+    // pagination
+    if (Pagination) {
+      pagination.forEach((item, index) => {
+        if (index == counter) {
+          pagination[index].classList.add("bg-[#fff]");
         } else {
-            next.style.opacity = "1"
-        } if (c == slide.length - 1) {
-            prev.style.opacity = "0.2"
-        } else {
-            prev.style.opacity = "1"
+          pagination[index].classList.remove("bg-[#fff]");
         }
-
-        // pagination
-        pages.forEach((page, index) => {
-
-
-            if (index === c) {
-
-                page.style.backgroundColor = "white";
-
-            } else {
-                page.style.backgroundColor = "black";
-            }
-        });
-
-
+      });
     }
 
+    // disable navigation
+    if (DisableNavigation) {
+      if (counter == 0) {
+        prev.style.opacity = 0.2;
+      } else {
+        prev.style.opacity = 1;
+      }
+      if (counter == slide.length - 1 - (slidesPerView - 1)) {
+        next.style.opacity = 0.2;
+      } else {
+        next.style.opacity = 1;
+      }
+    }
 
-    next.addEventListener("click", () => {
-        if (counter != 0) {
-            counter--;
-            counterslide(counter);
-            console.log("1");
-            
+    // min width 0 (button)
+    if (counter == 1) {
+      removeButton2.classList.add("!block");
+      removeButton1.classList.add("hidden");
+    } else {
+      removeButton2.classList.remove("!block");
+    }
+    if (counter == 0 || counter == 2 || counter == 3 || counter == 4) {
+      removeButton1.classList.add("!block");
+    } else {
+      removeButton1.classList.remove("!block");
+    }
+    if (counter > 4) {
+      removeButton1.parentElement.classList.add("!max-h-[70px]");
+    } else {
+      removeButton1.parentElement.classList.remove("!max-h-[70px]");
+    }
+  }
 
-        }
+  // loop
 
-    })
 
-    prev.addEventListener("click", () => {
-        if (counter != slide.length - 1) {
-            counter++;
-            counterslide(counter);
 
-        }
+  // navigation
+  next.addEventListener("click", () => {
+    if (counter != slide.length - 1 - (slidesPerView - 1)) {
+      prev.style.opacity = 1;
+      counter++;
+      counterSlider(counter);
+    }
+   
+    
+  });
+  prev.addEventListener("click", () => {
+    if (counter != 0) {
+      next.style.opacity = 1;
+      counter--;
+      counterSlider(counter);
+    }
+  });
 
-    })
+  // pagination
 
+  if (Pagination) {
+    [...pagination];
+    pagination.forEach((item, index) => {
+      pagination[index].addEventListener("click", () => {
+        counter = index;
+        counterSlider(counter);
+      });
+    });
+  }
 }
 
-export default t1Slider();
-
-
-
-// ----------------T1-Slider---------------------------
+export default createSlider
